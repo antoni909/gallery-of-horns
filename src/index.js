9025afce8b6a1781ee28ee17f2ef81f46e460d19
antoni <lorenzo.antonio.090@gmail.com>
2021-03-24 20:29:28 -0700

    add bootstrap to app dir

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '/Users/lorenzoortega/Desktop/codefellows/CF301-Folder/lab/gallery-of-horns/src/index.css';
 import App from './App';
 
--- src/js/HornedBeast.js
+++ src/js/HornedBeast.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class HornedBeast extends React.Component{
   constructor(props){
     super(props);
