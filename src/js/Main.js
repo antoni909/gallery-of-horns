6298bd765ee30b73a4cc5ead94788c7bc250bdf9
antoni <lorenzo.antonio.090@gmail.com>
2021-03-29 13:01:30 -0700

    restructure logic used to import data as a prop and openModal prop being passed to child component selectBeast

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
-import data from './data.json';
 import CardColumns from 'react-bootstrap/CardColumns';
 
 
 class Main extends React.Component{
   render(){
-
-    let beastArr = [];
-    data.forEach( (beast,idx) => {beastArr.push(
-
-      <HornedBeast
-        key={idx}
-        title={beast.title}
-        keyword={beast.keyword}
-        horns={beast.horns}
-        description={beast.description}
-        image_url={beast.image_url}
-      />
-
-    );
+    console.log(this.props.openModal);
+    let beastArr = this.props.dataSelectBeast.map( (beast,index) => {
+      return (
+        <div key={index}>
+          <HornedBeast
+            index={index}
+            openModal={this.props.openModal}
+            title={beast.title}
+            keyword={beast.keyword}
+            horns={beast.horns}
+            description={beast.description}
+            image_url={beast.image_url}
+          />;
+        </div> );
     });
-    return <CardColumns>{beastArr}</CardColumns>;
+    return (
+      <CardColumns>
+        {beastArr}
+      </CardColumns>);
   }
 }
 
